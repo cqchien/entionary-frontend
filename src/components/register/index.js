10cafa4cb2998034d873d7699f783f404b73f3e8
cqchien <caochientp1@gmail.com>
2021-08-23 23:07:28 +0700

    set register component

---
+++ src/components/register/index.js
@@ -0,0 +1,22 @@
+import { TextField } from "@material-ui/core";
+
+function Register() {
+  return (
+    <form className={"flex-col"}>
+      <div className="flex-col">
+        <h1 className="t-center">Create New Account</h1>
+        <div className="t-center mt-5">
+          Entionary
+        </div>
+      </div>
+
+      <div className="flex=col">
+        <TextField>
+          
+        </TextField>
+      </div>
+    </form>
+  );
+}
+
+export default Register;
