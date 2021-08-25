197f854634015bff37334e96bf5707c23454d41f
cqchien <caochientp1@gmail.com>
2021-08-26 01:47:04 +0700

    custom UI Login with google

---
+++ src/components/Login/SocialNetwork/Google.js
@@ -0,0 +1,24 @@
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+import googleIcon from "../../../assets/icons/gg-icon.png";
+import GoogleLogin from "react-google-login";
+import socialNetworkStyle from "./style";
+
+const useStyle = makeStyles(socialNetworkStyle);
+
+function Google() {
+  const classes = useStyle();
+  return (
+    <GoogleLogin
+      // Render prop to use a custom element, use renderProps.onClick
+      render={() => (
+        <div className={classes.socialBtn}>
+          <img className={classes.socialImg} src={googleIcon} alt="Google Icon" />
+          <span className={classes.socialName}>Google</span>
+        </div>
+      )}
+    />
+  );
+}
+
+export default Google;
---
+++ src/components/Login/SocialNetwork/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Google from "./Google";
+
+function SocialNetworkLogin() {
+  return (
+    <div className="d-flex" style={{ margin: "0 -0.8rem" }}>
+      <Google />
+    </div>
+  );
+}
+
+export default SocialNetworkLogin;
