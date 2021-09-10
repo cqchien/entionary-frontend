138b7ca4e3bfffa7e84b85bc168f1050e3192b2b
cqchien <caochientp1@gmail.com>
2021-09-10 10:09:31 +0700

    set up store and reducer

---
+++ src/redux/reducers/message.reducer.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Accepts an initial state, an object full of reducer functions, and a "slice name"
+// It uses createAction and createReducer -> use Immer to write "mutating" immutable updates:
+const messageReducer = createSlice({
+  // A name, used in action types
+  name: "message",
+  // The initial state for the reducer
+  initialState: {
+    open: false,
+    duration: 1500,
+    message: "",
+    variant: "filled",
+    type: "success",
+  },
+  // An object of "case reducers". Key names will be used to generate actions.
+  // Passed to createReducer, so the reducers may safely "mutate" the state they are given.
+  reducers: {
+    // type: message/setMessage
+    setMessage(state, { payload }) {
+      return { ...state, open: true, ...payload };
+    },
+  },
+});
+
+// Return values
+// {
+//   name : string,
+//   reducer : ReducerFunction,
+//   actions : Record<string, ActionCreator>,
+//   caseReducers: Record<string, CaseReducer>
+// }
+const { reducer, actions } = messageReducer;
+export const { setMessage } = actions;
+export default reducer;
