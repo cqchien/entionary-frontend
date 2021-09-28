a3233d417543bcf99978bffff7b8478c7cc2f564
cqchien <caochientp1@gmail.com>
2021-09-28 08:58:07 +0700

    refactor code

--- src/components/SocialNetwork/index.js
+++ src/components/SocialNetwork/index.js
@@ -1,32 +1,21 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { loginWithSocialNetwork } from "../../apis/account";
 import { ROUTES } from "../../constant/routePath";
-import { setMessage } from "../../redux/reducers/message.reducer";
 import Facebook from "./Facebook";
 import Google from "./Google";
 
 function SocialNetworkLogin() {
-  const dispatch = useDispatch();
   const history = useHistory();
 
   const handleLoginSocialNetwork = async (response) => {
     const { accessToken } = response;
-    try {
-      const apiResponse = await loginWithSocialNetwork({
-        access_token: accessToken,
-      });
-      const { success } = apiResponse;
-      if (success) {
-        history.push(ROUTES.HOME);
-      }
-    } catch (error) {
-      const payloadFail = {
-        message: error.response?.data?.message || " Error, Please try again!",
-        type: "error",
-      };
-      dispatch(setMessage(payloadFail));
+    const apiResponse = await loginWithSocialNetwork({
+      access_token: accessToken,
+    });
+    const success = apiResponse?.success;
+    if (success) {
+      history.push(ROUTES.HOME);
     }
   };
 
