1f10a243a09d90536c49212f97b529cb7b26a82d
cqchien <caochientp1@gmail.com>
2021-09-30 00:44:23 +0700

    NotFoudnPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import theme from "./configs/theme";
 import MessageAlert from "./containers/Message";
 import { routes } from "./configs/router";
 import Loading from "./components/Custom/Loading";
+import NotFoundPage from "./pages/NotFound";
 
 const extractRoutes = routes.map((route, index) => {
   const { path, exact, component } = route;
@@ -20,7 +21,12 @@ function App() {
       <BrowserRouter>
         {/* Suspense lets your components “wait” for something before they can render. */}
         <Suspense fallback={<Loading />}>
-          <Switch>{extractRoutes}</Switch>
+          <Switch>
+            {extractRoutes}
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Switch>
         </Suspense>
       </BrowserRouter>
       {/* <div id="_overlay"></div> */}
---
+++ src/pages/NotFound.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/not-found.scss";
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found">
+      <div className="not-found-main">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-content">
+          <b>Ooops... Page not found.</b> <br />
+          It looks like you are lost... That is a trouble?
+        </p>
+        <Link to="/">
+          <button type="button" className="not-found-btn">
+            Go Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
