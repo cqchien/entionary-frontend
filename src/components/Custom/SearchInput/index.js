060a2104ae7b45e36e51e4fb535515dffe98be80
cqchien <caochientp1@gmail.com>
2021-10-11 11:06:34 +0700

    Custom UI SearchInput

---
+++ src/components/Custom/SearchInput/index.js
@@ -0,0 +1,28 @@
+import { Grow, InputBase, makeStyles } from "@material-ui/core";
+import React from "react";
+import searchInputStyle from "./style";
+
+const useStyle = makeStyles(searchInputStyle);
+
+const SearchInputCustom = ({ placeholder, prefixIcon }) => {
+  const classes = useStyle();
+
+  return (
+    <div className={classes.nativeInput}>
+      <div className={`${classes.icon} flex-center show-input`}>
+        {prefixIcon}
+      </div>
+      <Grow in={true}>
+        <InputBase
+          onChange={(event) => console.log(event.target?.value)}
+          autoFocus
+          placeholder={placeholder}
+          classes={{ root: classes.inputRoot, input: classes.inputInput }}
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Grow>
+    </div>
+  );
+};
+
+export default SearchInputCustom;
