5edde09b86fb9f3f263c7dcb112e5d90f14947c5
cqchien <caochientp1@gmail.com>
2021-10-16 23:59:34 +0700

    homepage

---
+++ src/pages/home.js
@@ -0,0 +1,26 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import FeatureBox from "../components/FeatureBox";
+import { useTitle } from "../Hook/useTitle";
+
+const HomePage = () => {
+  useTitle("Entionary - Comprehensive English learning application");
+  return (
+    <div className="container">
+      <Grid container spacing={3}>
+        {FEATURE_LIST.map((box, index) => (
+          <Grid item xs={12} md={6} lg={4} key={index}>
+            <FeatureBox
+              imageUrl={box.imageUrl}
+              featureTitle={box.featureTitle}
+              urlToLink={box.urlToLink}
+              featureSubTitle={box.featureSubTitle}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default HomePage;
