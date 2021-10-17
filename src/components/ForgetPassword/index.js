3a4c51dc31bc253db211c64461247741e04b6189
cqchien <caochientp1@gmail.com>
2021-10-17 16:58:54 +0700

    ForgetPassword UI

---
+++ src/components/ForgetPassword/index.js
@@ -0,0 +1,149 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  makeStyles,
+} from "@material-ui/core";
+import { Loop, Visibility, VisibilityOff } from "@material-ui/icons";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import TextFieldCustom from "../Custom/TextField";
+import formStyle from "../globalStyle";
+
+const useStyle = makeStyles(formStyle);
+
+const ForgetPasswordForm = ({
+  validationSchema,
+  handleForgetPassword,
+  loading,
+  loadingSendCode,
+  handleSendCode,
+}) => {
+  const classes = useStyle();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const [isVisiblePass, changeVisiblePass] = useState(false);
+
+  return (
+    <form
+      className={`${classes.root} flex-col`}
+      onSubmit={handleSubmit(handleForgetPassword)}
+      autoComplete="off"
+    >
+      <h1 className={`${classes.title} t-center`}>Forget Password</h1>
+
+      <div className="flex-col">
+        <TextFieldCustom
+          label="Email"
+          size="small"
+          placeholder="Input email"
+          error={Boolean(errors.email)}
+          disabled={loading}
+          inputProps={{
+            ...register("email"),
+            autoFocus: true,
+          }}
+          helperText={errors.email?.message}
+        />
+      </div>
+
+      <div className="flex-col">
+        <div className="d-flex">
+          <TextFieldCustom
+            label="Verify Code"
+            size="small"
+            placeholder="Input Verify Code"
+            error={Boolean(errors.verifyCode)}
+            disabled={loading}
+            inputProps={{
+              ...register("verifyCode"),
+              autoFocus: true,
+            }}
+            helperText={errors.verifyCode?.message}
+          />
+          <Button
+            className="_btn _btn-stand ml-10"
+            disabled={loadingSendCode}
+            endIcon={loadingSendCode && <Loop className="ani-spin" />}
+            onClick={handleSendCode}
+          >
+            Send Code
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex-col">
+        <TextFieldCustom
+          label="Password"
+          size="small"
+          placeholder="Input Password"
+          error={Boolean(errors.password)}
+          helperText={errors.password?.message}
+          disabled={loading}
+          inputProps={{
+            ...register("password"),
+            type: isVisiblePass ? "text" : "password",
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={() => changeVisiblePass(!isVisiblePass)}>
+                {isVisiblePass ? (
+                  <Visibility className={`${classes.icon}`} />
+                ) : (
+                  <VisibilityOff className={`${classes.icon}`} />
+                )}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </div>
+
+      <div className="flex-col">
+        <TextFieldCustom
+          label="Confirm Password"
+          size="small"
+          placeholder="Input Password To Confirm"
+          error={Boolean(errors.confirmPassword)}
+          helperText={errors.confirmPassword?.message}
+          disabled={loading}
+          inputProps={{
+            ...register("confirmPassword"),
+            type: isVisiblePass ? "text" : "password",
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={() => changeVisiblePass(!isVisiblePass)}>
+                {isVisiblePass ? (
+                  <Visibility className={`${classes.icon}`} />
+                ) : (
+                  <VisibilityOff className={`${classes.icon}`} />
+                )}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </div>
+
+      <Button
+        className="_btn _btn-primary"
+        type="submit"
+        size="large"
+        disabled={loading}
+        //Element placed after the children.
+        endIcon={loading && <Loop className="ani-spin" />}
+      >
+        Change Password
+      </Button>
+    </form>
+  );
+};
+
+export default ForgetPasswordForm;
