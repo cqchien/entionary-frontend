3b9633e34d30e430a2c86d54a6afe079a5b081f2
cqchien <caochientp1@gmail.com>
2021-10-21 23:56:41 +0700

    update window.location.href

--- src/components/SocialNetwork/index.js
+++ src/components/SocialNetwork/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router";
 import { loginWithFacebook, loginWithGoogle } from "../../apis/account";
-import { ROUTES } from "../../constant/routePath";
 import { setLoading, setMessage } from "../../redux/reducers/message.reducer";
 import { setToken } from "../../utils/authority";
 import Facebook from "./Facebook";
 import Google from "./Google";
 
 function SocialNetworkLogin({ loading }) {
-  const history = useHistory();
   const dispatch = useDispatch();
 
   const handleLoginSocialNetwork = async (response) => {
@@ -25,7 +22,7 @@ function SocialNetworkLogin({ loading }) {
     const data = apiResponse?.data;
     if (success) {
       setToken(data.token);
-      history.push(ROUTES.HOME);
+      window.location.href = "/";
     } else {
       dispatch(setMessage(apiResponse));
     }
