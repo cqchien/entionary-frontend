fbed74b18d9e4b7c8b71e1b5335893ca03ceca4a
cqchien <caochientp1@gmail.com>
2021-11-08 22:23:58 +0700

    handle call api get flashcard with paginate

--- src/containers/Flashcards.js
+++ src/containers/Flashcards.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FlashcardsGallery from "../components/FlashcardsGallery";
-import houseImg from "../assets/images/house-flashcard.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { getFlashcards } from "../redux/reducers/flashcard.reducer";
 
 const Flashcards = () => {
-  const flashcardArr = [
-    {
-      name: "House",
-      picture: houseImg,
-      topic: "TOEIC",
-      mode: "PUBLIC",
-    },
-    {
-      name: "House",
-      picture: houseImg,
-      topic: "TOEIC",
-      mode: "PUBLIC",
-    },
-    {
-      name: "House",
-      picture: houseImg,
-      topic: "TOEIC",
-      mode: "PUBLIC",
-    },
-  ];
+  const dispatch = useDispatch();
+  const { loading, flashcards, pagination } = useSelector(
+    (state) => state.flashcard
+  );
 
-  return <FlashcardsGallery flashcardArr={flashcardArr} />;
+  const [paginateOptions, setPaginateOption] = useState({
+    page: 1,
+    take: 7,
+    sortBy: "",
+  });
+
+  useEffect(() => {
+    dispatch(getFlashcards(paginateOptions));
+    return () => {};
+  }, [dispatch, paginateOptions]);
+
+  return (
+    <FlashcardsGallery
+      loading={loading}
+      pagination={pagination}
+      flashcardArr={flashcards}
+    />
+  );
 };
 
 export default Flashcards;
