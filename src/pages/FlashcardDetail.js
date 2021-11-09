7aa0d12fd99221079b3ce6f572baa58a6a97170d
cqchien <caochientp1@gmail.com>
2021-11-10 01:42:17 +0700

    add tool tip in flashcard Detail

--- src/pages/FlashcardDetail.js
+++ src/pages/FlashcardDetail.js
@@ -1,9 +1,24 @@
+import { AddCircleOutlineOutlined } from "@material-ui/icons";
 import React from "react";
+import IconWrap from "../components/IconWrap";
 import FlashcardDetail from "../containers/FlashcardDetail";
+
+const toolTips = [
+  {
+    title: "Create FlashCard",
+    icon: <AddCircleOutlineOutlined />,
+  },
+];
 
 const FlashcardDetailPage = () => {
   return (
     <div className="container my-10">
+      <div className="flex-center-between">
+        <h1 className="entionary-title">Flashcard</h1>
+        <IconWrap toolTips={toolTips} />
+      </div>
+      <div className="entionary-break" />
+
       <FlashcardDetail />
     </div>
   );
