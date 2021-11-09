1d7fa942baeefeb2fc36344212c620193135d2c1
cqchien <caochientp1@gmail.com>
2021-11-09 11:16:09 +0700

    slide for words UI

---
+++ src/components/WordsSlide/index.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+import SlideItem from "../SlideItem";
+import wordsSlideStyle from "./style";
+
+const useStyle = makeStyles(wordsSlideStyle);
+
+const WordsSlide = ({ words }) => {
+  const classes = useStyle();
+  return (
+    <div className={`${classes.wrapper} flex-center--ver position-rel`}>
+      {words.map((word) => (
+        <SlideItem {...word} key={word.id} />
+      ))}
+    </div>
+  );
+};
+
+export default WordsSlide;
---
+++ src/components/WordsSlide/style.js
@@ -0,0 +1,14 @@
+const wordsSlideStyle = {
+  wrapper: {
+    height: "calc(100vh - 20rem)",
+  },
+
+  skeleton: {
+    borderTopLeftRadius: "64px",
+    borderBottomRightRadius: "64px",
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 0,
+  },
+};
+
+export default wordsSlideStyle;
