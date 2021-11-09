5ca284ce01b9ca06252f5802d9d9a76c0449e7f5
cqchien <caochientp1@gmail.com>
2021-11-09 23:59:40 +0700

    add loading in wordSlide

--- src/components/WordsSlide/index.js
+++ src/components/WordsSlide/index.js
@@ -1,17 +1,25 @@
 import { makeStyles } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import React from "react";
 import SlideItem from "../SlideItem";
 import wordsSlideStyle from "./style";
 
 const useStyle = makeStyles(wordsSlideStyle);
 
-const WordsSlide = ({ words }) => {
+const WordsSlide = ({ words, loading }) => {
   const classes = useStyle();
   return (
     <div className={`${classes.wrapper} flex-center--ver position-rel`}>
-      {words.map((word) => (
-        <SlideItem {...word} key={word._id} />
-      ))}
+      {!loading ? (
+        words?.map((word) => <SlideItem {...word} key={word._id} />)
+      ) : (
+        <Skeleton
+          variant="rect"
+          className={`${classes.skeleton} w-100`}
+          animation="wave"
+          style={{ height: "576px" }}
+        />
+      )}
     </div>
   );
 };
